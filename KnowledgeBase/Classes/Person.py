145d0003a3933bdfe7844993e81132f506a736f1
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2018-03-06 16:20:57 +0100

    persons are now more conform with btl datatype persondata

--- KnowledgeBase/Classes/Person.py
+++ KnowledgeBase/Classes/Person.py
@@ -4,20 +4,21 @@ import xml.etree.ElementTree as ET
 
 
 class Person(me.Document):
-    age = me.IntField(default=0)
+    ageFrom = me.IntField(default=0)
+    ageTo = me.IntField(default=0)
     gender = me.StringField(max_length=50, default='')
     shirtcolor = me.StringField(max_length=50, default='')
     pose = me.StringField(max_length=50, default='')
     gesture = me.StringField(max_length=50, default='')
     lastKnownPosition = me.EmbeddedDocumentField(Robotposition)
     name = me.StringField(max_length=100, default='')
+    uuid = me.StringField(max_length=50, required=True, primary_key=True)
+    faceId = me.IntField(default=-1)
     # pointcloud
 
     def to_xml(self):
         attribs = {x: self.__getattribute__(x) for x in self._fields}
         posi = attribs.pop('lastKnownPosition')
-        name = attribs.pop('name') #todo: remove
-        attribs.pop('id')
         attribs = {x: str(attribs[x]) for x in attribs}
 
         root = ET.Element('PERSON', attrib=attribs)
